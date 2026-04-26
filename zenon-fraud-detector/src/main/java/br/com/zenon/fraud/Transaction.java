79fc16630ea80c5a9a59e34365621b451653c212
Guilherme Santana <santana.guilherme93@gmail.com>
2026-04-26 00:26:55 -0300

    Implementação da modelagem dos dados

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Transaction.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Transaction.java
@@ -11,25 +11,6 @@ public record Transaction(
         int isFraud,
         int isFlaggedFraud
 ) {
-
-    public static Transaction fromCsv(
-            int step,
-            TransactioType type,
-            BigDecimal amount,
-            String nameOrig,
-            BigDecimal oldbalanceOrg,
-            BigDecimal newbalanceOrig,
-            String nameDest,
-            BigDecimal oldbalanceDest,
-            BigDecimal newbalanceDest,
-            int isFraud,
-            int isFlaggedFraud
-    ) {
-        OriginCustomer originCustomer = new OriginCustomer(nameOrig, oldbalanceOrg, newbalanceOrig);
-        DestCustomer destCustomer = new DestCustomer(nameDest, oldbalanceDest, newbalanceDest);
-        return new Transaction(step, type, amount, originCustomer, destCustomer, isFraud, isFlaggedFraud);
-    }
-
     @Override
     public String toString() {
         return "Transaction{" +
